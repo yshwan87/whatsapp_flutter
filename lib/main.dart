ced158f582cb67a18dfa749e1ed30e8d4adcdaed
yshwan87 <77893199+yshwan87@users.noreply.github.com>
2023-05-09 16:19:36 -0700

    Firebase set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_flutter/colors.dart';
+import 'package:whatsapp_flutter/firebase_options.dart';
 import 'package:whatsapp_flutter/screens/mobile_layout_screen.dart';
 import 'package:whatsapp_flutter/screens/web_layout_screen.dart';
 import 'package:whatsapp_flutter/utils/responsive_layout.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
