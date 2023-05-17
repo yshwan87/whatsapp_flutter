c3b6d2611204b936893717b1d5f2368ec0ac46c9
yshwan87 <77893199+yshwan87@users.noreply.github.com>
2023-05-17 15:37:40 -0700

    Saving user Data to Firestore

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_flutter/common/repositories/common_firebase_storage_repository.dart';
 import 'package:whatsapp_flutter/common/utils/utils.dart';
 import 'package:whatsapp_flutter/features/auth/screens/otp_screen.dart';
+import 'package:whatsapp_flutter/models/user_model.dart';
 
 import '../screens/user_information_screen.dart';
 
@@ -65,6 +69,46 @@ class AuthRepository {
       );
     } on FirebaseAuthException catch (e) {
       showSnackBar(context: context, content: e.message!);
+    }
+  }
+
+  void saveUserDataToFirebase({
+    required String name,
+    required File? profilePic,
+    required ProviderRef ref,
+    required BuildContext context,
+  }) async {
+    try {
+      String uid = auth.currentUser!.uid;
+      String photoUrl = 'https://png.pngitem.com/pimgs/s/649-6490124_katie-notopoulos-katienotopoulos-i-write-about-tech-round.png';
+
+      if (profilePic != null) {
+        photoUrl = await ref.read(commonFirebaseStorageRepositoryProvider).storeFileToFirebase(
+              'profilePic/$uid',
+              profilePic,
+            );
+      }
+
+      var user = UserModel(
+        name: name,
+        uid: uid,
+        profilePic: photoUrl,
+        isOnline: true,
+        phoneNumber: auth.currentUser!.phoneNumber!,
+        groupId: [],
+      );
+
+      await firestore.collection('users').doc(uid).set(user.toMap());
+
+      //   Navigator.pushAndRemoveUntil(
+      //     context,
+      //     MaterialPageRoute(
+      //       builder: (context) => const MobileLayoutScreen(),
+      //     ),
+      //     (route) => false,
+      //   );
+    } catch (e) {
+      showSnackBar(context: context, content: e.toString());
     }
   }
 }
--- lib/features/auth/screens/user_information_screen.dart
+++ lib/features/auth/screens/user_information_screen.dart
@@ -1,11 +1,94 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:whatsapp_flutter/common/utils/utils.dart';
 
-class UserInformationScreen extends StatelessWidget {
+class UserInformationScreen extends StatefulWidget {
   static const String routeName = '/user-information';
   const UserInformationScreen({super.key});
 
   @override
+  State<UserInformationScreen> createState() => _UserInformationScreenState();
+}
+
+class _UserInformationScreenState extends State<UserInformationScreen> {
+  final TextEditingController nameController = TextEditingController();
+
+  File? image;
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+  }
+
+  void selectImage() async {
+    image = await pickImageFromGallery(context);
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    image == null
+                        ? const CircleAvatar(
+                            backgroundImage: NetworkImage(
+                              'https://png.pngitem.com/pimgs/s/649-6490124_katie-notopoulos-katienotopoulos-i-write-about-tech-round.png',
+                            ),
+                            radius: 64,
+                          )
+                        : CircleAvatar(
+                            backgroundImage: FileImage(
+                              image!,
+                            ),
+                            radius: 64,
+                          ),
+                    Positioned(
+                      bottom: -10,
+                      left: 80,
+                      child: IconButton(
+                        onPressed: selectImage,
+                        icon: const Icon(
+                          Icons.add_a_photo,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: size.width * 0.85,
+                      padding: const EdgeInsets.all(20),
+                      child: TextField(
+                        controller: nameController,
+                        decoration: const InputDecoration(
+                          hintText: 'Enter your name',
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.done,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
