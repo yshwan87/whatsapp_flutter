b7e2096c4aaac656835eb9028886723be043b2dd
yshwan87 <77893199+yshwan87@users.noreply.github.com>
2023-05-30 15:58:23 -0700

    Scrolling on new message and changing online / offline status

--- lib/features/chat/widgets/chat_list.dart
+++ lib/features/chat/widgets/chat_list.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:whatsapp_flutter/common/widgets/loader.dart';
@@ -8,7 +9,7 @@ import 'package:whatsapp_flutter/models/message.dart';
 import 'package:whatsapp_flutter/widgets/my_message_card.dart';
 import 'package:whatsapp_flutter/widgets/sender_message_card.dart';
 
-class ChatList extends ConsumerWidget {
+class ChatList extends ConsumerStatefulWidget {
   final String recieverUserId;
   const ChatList({
     Key? key,
@@ -16,14 +17,32 @@ class ChatList extends ConsumerWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _ChatListState();
+}
+
+class _ChatListState extends ConsumerState<ChatList> {
+  final ScrollController messageController = ScrollController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    messageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return StreamBuilder<List<Message>>(
-        stream: ref.read(chatControllerProvider).chatStream(recieverUserId),
+        stream: ref.read(chatControllerProvider).chatStream(widget.recieverUserId),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Loader();
           }
+          SchedulerBinding.instance.addPostFrameCallback((_) {
+            messageController.jumpTo(messageController.position.maxScrollExtent);
+          });
+
           return ListView.builder(
+            controller: messageController,
             itemCount: snapshot.data!.length,
             itemBuilder: (context, index) {
               final messageData = snapshot.data![index];
